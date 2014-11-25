9e0f2ca8f3af6b16d4088b7ed2e01f80c748c1ef
Jorden Lowe <supermitsuba@gmail.com>
2014-11-24 21:10:12 -0500

    created different colors
    created different colors for the different categories

--- public/js/model/note.js
+++ public/js/model/note.js
@@ -4,4 +4,15 @@ function Note (name, comment, createdDate, modifiedDate, category) {
     this.createdDate = createdDate;
     this.modifiedDate = modifiedDate;
     this.category = category;
+    this.categoryClass = function(){
+        if(this.category === 'work'){
+            return "panel panel-danger";
+        }
+        else if(this.category === 'home'){
+            return "panel panel-success";
+        }
+        else{
+            return "panel panel-info";
+        }
+    }
 }
